2bbc5653c05de73e5f084533e610ed2b1eb9d129
Sam <63341022+whereissam@users.noreply.github.com>
2021-01-19 14:29:02 +0800

    Vue form

---
+++ vue-form/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+ <SignupForm />
+</template>
+
+<script>
+import SignupForm from './components/Signupform.vue'
+
+export default {
+  name: 'App',
+  components: { SignupForm }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+body{
+  margin: 0;
+  background-color: #eee;
+}
+
+</style>
