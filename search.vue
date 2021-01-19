a49de3218d91d7690a27305c2bfa5455b70dba58
Sam <63341022+whereissam@users.noreply.github.com>
2021-01-19 17:29:48 +0800

    Composition API

---
+++ search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+  <h1>home</h1>
+  <input type="text" v-model="search">
+  <p>Search: {{search}}</p>
+  <div v-for="name in matchingName" :key='name'>{{name}}</div>
+  </div>
+</template>
+
+<script>
+import { computed,ref, watch, watchEffect} from 'vue'
+
+export default {
+  name: 'Home',
+  setup(){
+    const search = ref('')
+    const names = ref(['mario','fdjasif', 'fdiao','ieo','jioe'])
+
+    watch(search,()=>{
+      console.log('watch function run')
+    })
+
+    watchEffect(()=>{
+      console.log('watcheffect function run',search.value)
+    }) //only watch one time
+    const matchingName = computed(()=>{
+      return names.value.filter((name) =>
+        name.includes(search.value)
+      )
+    })
+
+    return { names,search, matchingName }
+  }
+}
+</script>
